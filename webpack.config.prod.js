127eb6c5a2095b41fba2311ef63deee6f58a4216
VasiliyHarden <generaleprotein@gmail.com>
2020-07-14 19:56:25 +0400

    environment setup

---
+++ webpack.config.prod.js
@@ -0,0 +1,39 @@
+const path = require('path');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+  context: path.resolve(__dirname, 'src'),
+  entry: path.resolve(__dirname, 'src', 'index.js'),
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'build')
+  },
+  mode: "production",
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HTMLWebpackPlugin({
+      template: 'index.html'
+    }),
+    new CopyWebpackPlugin({ 
+      patterns: [ { from: './assets', to: 'assets' } ]
+    }),
+    new MiniCssExtractPlugin({
+      filename: 'bundle.css'
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          'sass-loader'
+        ]
+      }
+    ]
+  }
+}
